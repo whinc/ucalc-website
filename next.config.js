445607c832637863307136aefc27cae541b754fb
whincwu <whincwu@163.com>
2024-03-10 17:41:37 +0800

    fix ci

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ module.exports = withBundleAnalyzer({
   },
   poweredByHeader: false,
   trailingSlash: true,
-  basePath: 'uclac-website',
+  basePath: '/uclac-website',
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
