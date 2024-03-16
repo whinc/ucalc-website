ec1623cae012c644845d99d8a70ad26812f32f73
whincwu <whincwu@163.com>
2024-03-16 10:58:29 +0800

    add 构建产物

---
+++ out/_next/static/MbkH9cm8CkmT3LIzq08iv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/803-43d46e01f6cc2559.js","static/chunks/pages/index-a120dc4473ee9ba0.js"],"/_error":["static/chunks/pages/_error-b9e1de59d6b43160.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
