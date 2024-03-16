5554c0544e8f6a79b13b68d355236b3eacb70c78
whincwu <whincwu@163.com>
2024-03-16 11:33:35 +0800

    add gitee pages

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # 微信小程序【小而美工具】
 
+[官网站点](https://whinc.github.io/ucalc-website/) | [备用站点（国内推荐）](https://xiaohui_hubei.gitee.io/ucalc-website/)
+
+微信号：`iCloudCoding`
+
 <p align="center">
   <a href="https://whinc.github.io/ucalc-website/" target="_blank"><img src="./public/assets/images/qrcode_soso.png" alt="Next js starter banner"></a>
 </p>
-
-微信号：`iCloudCoding`
 
 ### 本地开发
 
--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { useEffect, useMemo, useState } from 'react';
 
 import { AppConfig } from '@/utils/AppConfig';
 
@@ -8,53 +9,89 @@ import { HeroOneButton } from '../hero/HeroOneButton';
 import { Section } from '../layout/Section';
 import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
 import { Logo } from './Logo';
+import { hostname } from 'os';
 
-const Hero = () => (
-  <Background color="bg-gray-100">
-    <Section yPadding="py-6">
-      <NavbarTwoColumns logo={<Logo xl />}>
-        <li>
-          <Link href="https://github.com/whinc/ucalc-website" target={'_blank'}>
-            GitHub
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="https://github.com/whinc/ucalc-website/issues/new"
-            target={'_blank'}
-          >
-            意见反馈
-          </Link>
-        </li>
-      </NavbarTwoColumns>
-    </Section>
+const Hero = (props) => {
+  const [hostname, setHostName] = useState('');
+  useEffect(() => {
+    setHostName(window.location.hostname);
+  }, []);
 
-    <Section yPadding="pt-20 pb-32">
-      <HeroOneButton
-        title={
-          <>
-            {'一款有温度的小程序\n'}
-            <span className="text-primary-500">小而美工具</span>
-          </>
-        }
-        description={AppConfig.description}
-        button={null}
-        // button={
-        //   <Link href="https://creativedesignsguru.com/category/nextjs/">
-        //     <Button xl>立即体验</Button>
-        //   </Link>
-        // }
-      />
-      <Image
-        className="m-auto"
-        src={`${AppConfig.basePath}/assets/images/qrcode_soso.png`}
-        alt="小而美工具小程序码"
-        width={543}
-        height={200}
-        loading="lazy"
-      />
-    </Section>
-  </Background>
-);
+  return (
+    <Background color="bg-gray-100">
+      <Section yPadding="py-6">
+        <NavbarTwoColumns logo={<Logo xl />}>
+          <li>
+            {hostname === 'whinc.github.io' ? (
+              <Link
+                href="https://xiaohui_hubei.gitee.io/ucalc-website/"
+                target={'_blank'}
+              >
+                国内站点
+              </Link>
+            ) : (
+              <Link
+                href="https://whinc.github.io/ucalc-website/"
+                target={'_blank'}
+              >
+                国际站点
+              </Link>
+            )}
+          </li>
+          <li>
+            <Link
+              href="https://github.com/whinc/ucalc-website"
+              target={'_blank'}
+            >
+              GitHub
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="https://github.com/whinc/ucalc-website/issues/new"
+              target={'_blank'}
+            >
+              意见反馈
+            </Link>
+          </li>
+        </NavbarTwoColumns>
+      </Section>
+
+      <Section yPadding="pt-20 pb-32">
+        <HeroOneButton
+          title={
+            <>
+              {'一款有温度的小程序\n'}
+              <span className="text-primary-500">小而美工具</span>
+            </>
+          }
+          description={AppConfig.description}
+          button={null}
+          // button={
+          //   <Link href="https://creativedesignsguru.com/category/nextjs/">
+          //     <Button xl>立即体验</Button>
+          //   </Link>
+          // }
+        />
+        <Image
+          className="m-auto"
+          src={`${AppConfig.basePath}/assets/images/qrcode_soso.png`}
+          alt="小而美工具小程序码"
+          width={543}
+          height={200}
+          loading="lazy"
+        />
+      </Section>
+    </Background>
+  );
+};
+
+export async function getStaticProps(context) {
+  return {
+    props: {
+      getWindow: () => window,
+    },
+  };
+}
 
 export { Hero };
