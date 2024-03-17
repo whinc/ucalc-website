46c7b4f68e720c81a035d81e6d58c445f3e4b790
whincwu <whincwu@163.com>
2024-03-17 11:06:07 +0800

    build for gitee

--- out/_next/static/qG87Jwde-p2XRUdCz_RvE/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/803-43d46e01f6cc2559.js","static/chunks/pages/index-14cf1d4f9caedaf7.js"],"/_error":["static/chunks/pages/_error-b9e1de59d6b43160.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
