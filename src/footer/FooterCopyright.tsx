9685baedf6a486488c230440159b168317b80f8f
whincwu <whincwu@163.com>
2024-10-20 20:11:03 +0800

    fix css error

--- src/footer/FooterCopyright.tsx
+++ src/footer/FooterCopyright.tsx
@@ -3,27 +3,14 @@ import { AppConfig } from '../utils/AppConfig';
 const FooterCopyright = () => (
   <div className="footer-copyright">
     © Copyright {new Date().getFullYear()} {AppConfig.title}。 由{' '}
-    <a href="https://github.com/whinc" target={'_blank'}>
+    <a
+      className="text-sky-500"
+      href="https://github.com/whinc"
+      target={'_blank'}
+    >
       右耳朵猫
     </a>
     （微信号：<a>iCloudCoding</a>）开发。
-    {/*
-     * PLEASE READ THIS SECTION
-     * I'm an indie maker with limited resources and funds, I'll really appreciate if you could have a link to my website.
-     * The link doesn't need to appear on every pages, one link on one page is enough.
-     * For example, in the `About` page. Thank you for your support, it'll mean a lot to me.
-     */}
-    <style jsx>
-      {`
-        .footer-copyright :global(a) {
-          @apply text-primary-500;
-        }
-
-        .footer-copyright :global(a:hover) {
-          @apply underline;
-        }
-      `}
-    </style>
   </div>
 );
 
